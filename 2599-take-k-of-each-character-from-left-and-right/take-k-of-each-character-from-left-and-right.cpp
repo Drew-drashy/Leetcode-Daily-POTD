f9beae7d975048b3d483138c87288029822ae767
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2024-11-21 01:34:06 +0530

    Time: 11 ms (71.48%) | Memory: 12 MB (80.20%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int n=s.size();
-        if(k==0) return k;
-        if(n<3) return -1;
-        int i=0;
-        int j=0;
-        ;
-        vector<int>dp(3,0);
-        for(char i: s) dp[i-'a']++;
-        int mini=INT_MAX;
-        while(i<=j && j<n){
-            if(dp[0]>=k && dp[1]>=k && dp[2]>=k){
-                mini=min(mini,n-j+i);
-                dp[s[j++]-'a']--;
-            }
-            else{
-                dp[s[i++]-'a']++;
-            }
+       int n=s.size();
+       int cnt[3]={0};
+       for(char i: s){
+        cnt[i-'a']++;
+       }
+       if(min({cnt[0],cnt[1],cnt[2]})<k) return -1;
+       int res=n;
+       int j=0;
+       for(int i=0;i<n;i++){
+        while(j<n && cnt[s[j]-'a']>k){
+            cnt[s[j++]-'a']--;
+
         }
-        if( i<=j && dp[0]>=k && dp[1]>=k && dp[2]>=k){
-                mini=min(mini,n-j+i);
-                // dp[s[j++]-'a']++;
-        }
-        return mini==INT_MAX? -1: mini;
+        res=min(res,i+n-j);
+        cnt[s[i]-'a']++;
+       }
+       return res;
 
     }
 };
+
+// class Solution {
+// public:
+//     int takeCharacters(string s, int k) {
+//         int n = s.size(), cnt[3] = {};
+
+//         for (char c : s) {
+            
+//             cnt[c-'a']++;
+//         }
+//         if (min({cnt[0], cnt[1], cnt[2]}) < k) return -1;
+
+//         int res = n;
+//         for (int i = 0, j = 0; i < n; i++) {
+//             while (j < n && cnt[s[j]-'a'] > k)
+//                 cnt[s[j++]-'a']--;
+//             res = min(res, i + (n - j));
+//             cnt[s[i]-'a']++;
+//         }
+
+//         return res;
+//     }
+// };
