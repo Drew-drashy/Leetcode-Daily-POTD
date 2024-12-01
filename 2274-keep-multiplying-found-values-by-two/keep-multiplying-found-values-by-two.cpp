b6429602697237359b3b978880614bca59421721
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2024-12-01 20:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (26.56%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        map<int,int>mp;
-        for(int i: nums) mp[i]++;
+        set<int>mp;
+        for(int i: nums) mp.insert(i);
         while(mp.find(original)!=mp.end()){
             original*=2;
         }
