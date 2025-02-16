b923c64b3a8bc55d43b793ad5444924e4c6c1ec6
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-02-16 15:17:21 +0530

    Time: 2 ms (33.33%) | Memory: 8.6 MB (36.80%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,40 +1,68 @@
 class Solution {
 public:
-    vector<int> constructDistancedSequence(int n) {
-        vector<int> result(2 * n - 1, 0);
-        vector<bool> used(n + 1, false);
-        backtrack(result, used, n, 0);
-        return result;
-    }
 
-private:
-    bool backtrack(vector<int>& result, vector<bool>& used, int n, int index) {
-        while (index < result.size() && result[index] != 0) {
-            index++;
-        }
-        if (index == result.size()) {
-            return true;
-        }
+    bool fun(int i, int n, vector<int>&result,vector<bool>&used){
+        int len=result.size();
+        if(i>=len) return true;
+        if(result[i]!=0) return fun(i+1,n,result,used);
+        for(int ind=n;ind>=1;ind--){
+            if(used[ind])  continue;
 
-        for (int i = n; i >= 1; i--) {
-            if (used[i]) continue;
-
-            if (i == 1) {
-                result[index] = 1;
-                used[1] = true;
-                if (backtrack(result, used, n, index + 1)) return true;
-                result[index] = 0;
-                used[1] = false;
-            } else if (index + i < result.size() && result[index + i] == 0) {
-                result[index] = i;
-                result[index + i] = i;
-                used[i] = true;
-                if (backtrack(result, used, n, index + 1)) return true;
-                result[index] = 0;
-                result[index + i] = 0;
-                used[i] = false;
+            if(ind==1){
+                result[i]=ind;
+                used[ind]=true;
+                if(fun(i+1,n,result,used)) return true;
+                result[i]=0;
+                used[1]=false;
+            }
+            else if(i+ind<len && result[i+ind]==0){
+                result[i]=ind;
+                result[i+ind]=ind;
+                used[ind]=true;
+                if(fun(i+1,n,result,used))return true;
+                result[i]=0;
+                result[i+ind]=0;
+                used[ind]=false;
             }
         }
         return false;
     }
+    vector<int> constructDistancedSequence(int n) {
+        vector<int> result(2 * n - 1, 0);
+        vector<bool> used(n + 1, false);
+        // backtrack(result, used, n, 0);
+        bool k=fun(0,n,result,used);
+        return result;
+    }
+
+private:
+    // bool backtrack(vector<int>& result, vector<bool>& used, int n, int index) {
+    //     while (index < result.size() && result[index] != 0) {
+    //         index++;
+    //     }
+    //     if (index == result.size()) {
+    //         return true;
+    //     }
+
+    //     for (int i = n; i >= 1; i--) {
+    //         if (used[i]) continue;
+
+    //         if (i == 1) {
+    //             result[index] = 1;
+    //             used[1] = true;
+    //             if (backtrack(result, used, n, index + 1)) return true;
+    //             result[index] = 0;
+    //             used[1] = false;
+    //         } else if (index + i < result.size() && result[index + i] == 0) {
+    //             result[index] = i;
+    //             result[index + i] = i;
+    //             used[i] = true;
+    //             if (backtrack(result, used, n, index + 1)) return true;
+    //             result[index] = 0;
+    //             result[index + i] = 0;
+    //             used[i] = false;
+    //         }
+    //     }
+    //     return false;
+    // }
 };
