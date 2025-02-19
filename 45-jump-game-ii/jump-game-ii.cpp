f726032b5bd44a54a05a921e704240741aab0e59
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-02-20 00:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (61.85%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    long long fun(int ind,vector<int>&nums,vector<long long>&dp){
-        // cout<<"hi";
-        int n=nums.size();
-        if(ind>=n) return 0;
-        if(ind==n-1) return 1;
-        if(dp[ind]!=-1) return dp[ind];
-        long long  jump=INT_MAX;
-        for(int i=1;i<=nums[ind];i++){
-
-            if(i+ind<n){
-                long long temp=1+fun(i+ind,nums,dp);
-                jump=min(jump,temp);
-            }
-        }
-        return dp[ind]=jump;
-    }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<long long >dp(n,-1);
-        return fun(0,nums,dp)-1;
 
+        int l=0;
+        int r=0;
+        int jumps=0;
+
+        while(r < n-1){
+
+            int fastest=0;
+
+            for(int i=l;i<=r;i++){
+                fastest=max(i+nums[i],fastest);
+            }
+
+            l=r+1;
+            r=fastest;
+            jumps+=1;
+        }
+        return jumps;
     }
 };
