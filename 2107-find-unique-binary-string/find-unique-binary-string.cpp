d9d05fcec5ed025fe865ceb1c0aad55a5ff8e6c5
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-02-20 10:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (84.00%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -14,15 +14,22 @@ public:
     }
     string findDifferentBinaryString(vector<string>& nums) {
         int n=nums.size();
-        map<string,int>mp;
-        for(string i: nums) mp[i]++;
-        int m=nums[0].size();
+        // map<string,int>mp;
+        // for(string i: nums) mp[i]++;
+        // int m=nums[0].size();
+        // string res="";
+        // fun(0,n,res,mp);
+        // // for(string l: ans) cout<<l;
+        // for(string k: ans){
+        //     if(mp.find(k)==mp.end()) return k;
+        // }
+        // return "";
+
+        // now flip the ith bit so how i conclude to this.
         string res="";
-        fun(0,n,res,mp);
-        // for(string l: ans) cout<<l;
-        for(string k: ans){
-            if(mp.find(k)==mp.end()) return k;
+        for(int i=0;i<n;i++){
+            res+=(nums[i][i]=='0')? "1": "0";
         }
-        return "";
+        return res;
     }
 };
