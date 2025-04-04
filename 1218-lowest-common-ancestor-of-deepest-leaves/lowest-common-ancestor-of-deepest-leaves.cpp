7b91adccbc38e71e953d1d9248e2cf574ac17a94
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-04-04 23:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (39.60%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,23 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* lcaDeepestLeaves(TreeNode* root) { 
-        return dfs(root).second; 
+    pair<int,TreeNode*> solve(TreeNode* root){
+        if(root==NULL) return {0,NULL};
+        auto l=solve(root->left);
+        auto r=solve(root->right);
+        if(l.first==r.first) return {l.first+1,root};
+        else if(l.first>r.first) return {l.first+1,l.second};
+        else return {r.first+1,r.second};
     }
-
-    // Helper function that returns a pair of (depth, lca node)
-    pair<int, TreeNode*> dfs(TreeNode* root) {
-        if (!root)
-            return {0, nullptr};
-
-        auto left = dfs(root->left);
-        auto right = dfs(root->right);
-
-        if (left.first == right.first) {
-            return {left.first + 1, root}; // current node is LCA
-        } else if (left.first > right.first) {
-            return {left.first + 1, left.second};
-        } else {
-            return {right.first + 1, right.second};
-        }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return solve(root).second;
     }
 };
