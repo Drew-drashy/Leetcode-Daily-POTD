e49de1ad5e3a14e72cef44517aff2792747084b3
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-05-04 13:29:18 +0530

    Time: 9 ms (31.00%) | Memory: 28.4 MB (17.75%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -5,8 +5,8 @@ public:
         int cnt=0;
         map<pair<int,int>,int>mp;
         for(auto it: dominoes){
-            if(it[0]>it[1]) swap(it[0],it[1]);
-            cnt+=mp[{it[0],it[1]}]++;
+            // if(it[0]>it[1]) swap(it[0],it[1]);
+            cnt+=mp[{max(it[0],it[1]),min(it[0],it[1])}]++;
         }
         return cnt;
     }
