57b0a0397f1860a65f046af597689b965e9ee53f
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-05-22 15:24:07 +0530

    Time: 412 ms (62.13%) | Memory: 160.5 MB (82.35%) - LeetSync

--- 3847-minimum-swaps-to-sort-by-digit-sum/minimum-swaps-to-sort-by-digit-sum.cpp
+++ 3847-minimum-swaps-to-sort-by-digit-sum/minimum-swaps-to-sort-by-digit-sum.cpp
@@ -11,40 +11,34 @@ public:
 
     int minSwaps(vector<int>& nums) {
         int n = nums.size();
-        vector<int> v = nums;
+       vector<pair<int,int>>arr;
+       vector<int>visited(n,0);
+       for(int i=0;i<n;i++){
+        arr.push_back({nums[i],i});
 
-        sort(v.begin(), v.end(), [&](int a, int b) {
-            int sumA = findsum(a);
-            int sumB = findsum(b);
-            if (sumA == sumB) return a < b;
-            return sumA < sumB;
-        });
+       }
+       sort(arr.begin(),arr.end(),[&](auto a,auto b){
+        int sum1=findsum(a.first);
+        int sum2=findsum(b.first);
+        if(sum1==sum2) return a.first<b.first;
+        return sum1<sum2;
+       });
+       int cnt=0;
+       for(int i=0;i<n;i++){
+            if(visited[i] || arr[i].second==i) continue;
 
-        unordered_map<int, int> mp;
-        for (int i = 0; i < n; ++i) {
-            mp[nums[i]] = i;
-        }
-
-        vector<bool> visited(n, false);
-        int cnt = 0;
-
-        for (int i = 0; i < n; ++i) {
-            if (visited[i] || v[i] == nums[i]) continue;
-
-            int cycle_size = 0;
-            int j = i;
-
-            while (!visited[j]) {
-                visited[j] = true;
-                j = mp[v[j]];
-                cycle_size++;
+            int j=i;
+            int cycle=0;
+            while(!visited[j]){
+                visited[j]=1;
+                j=arr[j].second;
+                cycle++;
             }
-
-            if (cycle_size > 1) {
-                cnt += (cycle_size - 1);
+            if(cycle>1){
+                cnt+=(cycle-1);
             }
-        }
-
-        return cnt;
+       }
+       return cnt;
+       
     }
 };
