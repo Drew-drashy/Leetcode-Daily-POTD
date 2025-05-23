8435b2c159717c37ea76ac6ab638ccc05f1f2aa7
Drashy Sesodia <107747111+Drew-drashy@users.noreply.github.com>
2025-05-23 11:42:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (8.91%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int fun(int ind, int n,vector<int>&nums,vector<int>&dp){
-        if(ind>n){
-            return 0;
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        int take=nums[ind]+fun(ind+2,n,nums,dp);
-        int ntake=fun(ind+1,n,nums,dp);
+    int robRange(vector<int>& nums, int start, int end) {
+        int n = end - start + 1;
+        if (n == 0) return 0;
+        if (n == 1) return nums[start];
 
-        return dp[ind]=max(take,ntake);
+        vector<int> dp(n, 0);
+        dp[n - 1] = nums[end];
+        dp[n - 2] = max(nums[end - 1], nums[end]);
+
+        for (int i = n - 3; i >= 0; --i) {
+            dp[i] = max(nums[start + i] + dp[i + 2], dp[i + 1]);
+        }
+
+        return dp[0];
     }
+
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int>dp(n,-1);
-        vector<int>sp(n,-1);
-        return max(fun(0,n-2,nums,dp), fun(1,n-1,nums,sp));
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        // Case 1: Exclude last house
+        int option1 = robRange(nums, 0, n - 2);
+        // Case 2: Exclude first house
+        int option2 = robRange(nums, 1, n - 1);
+
+        return max(option1, option2);
     }
 };
